6554d2ec7078fdcb5d3287e349786ae864b5e34c
Yuhang Zhao <yuhang.zhao@verizon.com>
2015-05-01 17:54:37 -0400

    update blueprint

--- bit-on-bytes.go
+++ bit-on-bytes.go
@@ -1,8 +1,28 @@
 package bitonbytes
 
+import (
+	"errors"
+	"log"
+)
+
 func ShiftLeft(in []byte, distance int) ([]byte, error) {
+	// distance is the nubmer of bits to be shifted
 	l := len(in)
-	out := make([]byte, l)
-	out = in
+	log.Println(string(in))
+	log.Println(distance)
+	if l < distance {
+		return nil, errors.New("Shift distance " + distance + " is bigger than the length of byte array: " + l)
+	}
+	if l == distance {
+		out := make([]byte, l)
+		return out, nil
+	}
+
+	div := l / 8
+	res := l % 8
+	if res != 0 {
+		ll := div + 1
+	}
+	holder := make([]byte, ll)
 	return out, nil
 }
