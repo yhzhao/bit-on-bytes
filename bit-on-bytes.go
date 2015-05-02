2b8feedd40a7b6b69c63762e3c0e9c8238bfed88
yhzhao <johannzhaojohann@gmail.com>
2015-05-02 18:40:23 -0400

    first correct version

--- bit-on-bytes.go
+++ bit-on-bytes.go
@@ -9,8 +9,9 @@ func ShiftByte(in []byte, distance int) ([]byte, error) {
 	// distance is the number of bytes to be shifted.
 	// if positive, shift to the right
 	// if negative, shift to the left
+	l := len(in)
 	if l < distance {
-		return nil, errors.New("Shift distance " + distance + " is bigger than the length of byte array: " + len(in))
+		return nil, errors.New("Shift distance " + string(distance) + " is bigger than the length of byte array: " + string(l))
 	}
 	out := make([]byte, len(in))
 	if distance > 0 {
@@ -19,7 +20,7 @@ func ShiftByte(in []byte, distance int) ([]byte, error) {
 		}
 	}
 	if distance < 0 {
-		for i := 0; i < 0-distance; i++ {
+		for i := 0; i < l+distance; i++ {
 			out[i] = in[0-distance+i] // [1,2,3] > [2,3,0], distance = -1
 		}
 	}
@@ -28,22 +29,23 @@ func ShiftByte(in []byte, distance int) ([]byte, error) {
 
 func ShiftLeft(in []byte, distance int) ([]byte, error) {
 	// distance is the nubmer of bits to be shifted
+	var ll int
 	l := len(in)
 	div := l / 8
 	res := l % 8
 	if res != 0 {
-		ll := div + 1
+		ll = div + 1
 	}
 	log.Println(string(in))
 	log.Println(distance)
 	if l < ll {
-		return nil, errors.New("Shift distance " + distance + " is bigger than the length of byte array: " + l + " bytes")
+		return nil, errors.New("Shift distance " + string(distance) + " is bigger than the length of byte array: " + string(l) + " bytes")
 	}
+	out := make([]byte, l)
 	if l == ll {
-		out := make([]byte, l)
 		return out, nil
 	}
 
-	holder := make([]byte, ll)
+	//holder := make([]byte, ll)
 	return out, nil
 }
